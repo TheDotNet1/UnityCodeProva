0501892f2b2878fec9167741e407b0aa1a8882fc
TheDotNet1 <98549794+TheDotNet1@users.noreply.github.com>
2023-01-07 11:08:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import os
 
 def main():
     cc = input("Quanti terminali vuoi aprire? ")
+    
+    ccint = int(cc)
 
-    for i in cc:
+    for i in ccint:
         os.system("cmd.exe")
 
 if __name__ == "__main__":
